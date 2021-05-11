5dd622eedc5d50eb16376c9d1336372fd6055c25
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-05-10 21:24:45 -0600

    added mapping to search. updated api calls

--- clients/src/utils/API.js
+++ clients/src/utils/API.js
@@ -3,18 +3,22 @@ import axios from "axios";
 export default {
     
     searchBooks: function(book) {
-      return axios.get("https://www.googleapis.com/books/v1/volumes?q="+book);
+      return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book)
     },
     
     saveBook: function(bookData) {
       return axios.get("/api/books", bookData);
     },
 
-    findBooks : function(){
+    findBooks: function(){
         return axios.get("/api/books");
     },
+
+    getBook: function(id) {
+      return axios.delete("/api/books/" + id);
+    },
     
-    deletePost: function(id) {
+    deleteBook: function(id) {
       return axios.delete("/api/books/" + id);
     },
    
---
+++ clients/src/utils/BooksMapping.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const BooksMapped = React.createContext ({
+    _id: 0,
+    title: "",
+    author: "",
+    description: "",
+    date: "",
+    image: "",
+    link: "",
+       
+
+})
+
+export default BooksMapped;
