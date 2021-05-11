ebeffeb6e8952188ea87741bc50077b66eeeb2be
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-05-10 21:08:29 -0600

    cleaning up error in code.

--- controllers/books-controller.js
+++ controllers/books-controller.js
@@ -5,27 +5,27 @@ module.exports = {
   findAll: function(req, res) {
     db.Book.find()
       .sort({date:-1})     
-      .then(dbModel => res.json(dbModel))
+      .then(result => res.json(result))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
     db.Book.findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
+      .then(result => res.json(result))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
     db.Book.create(req.body)
-      .then(dbModel => res.json(dbModel))
+      .then(result => res.json(result))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
     db.Book.findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
+      .then(result => res.json(result))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
     db.Book.findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())      
+      .then(result => result.remove())      
       .catch(err => res.status(422).json(err));
   }
 };
